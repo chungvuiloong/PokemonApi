d564d73e51a562745e4f581d2cb174107de1bdf4
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-03-12 16:37:30 +0200

    commit

--- main.js
+++ main.js
@@ -1,90 +1,56 @@
-// class FetchWrapper {
-//   constructor(baseURL) {
-//     this.baseURL = baseURL;
-//   }
-
-//   get(endpoint) {
-//     return fetch(this.baseURL + endpoint).then((response) => response.json());
-//   }
-
-//   put(endpoint, body) {
-//     return this._send("put", endpoint, body);
-//   }
-
-//   post(endpoint, body) {
-//     return this._send("post", endpoint, body);
-//   }
-
-//   patch(endpoint, body) {
-//     return this._send("patch", endpoint, body);
-//   }
-
-//   delete(endpoint, body) {
-//     return this._send("delete", endpoint, body);
-//   }
-
-//   _send(method, endpoint, body) {
-//     return fetch(this.baseURL + endpoint, {
-//       method,
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify(body),
-//     }).then((response) => response.json());
-//   }
-// }
-
-// const API = new FetchWrapper("https://pokeapi.co/api/v2/pokemon/");
-
-// console.log(API.count);
-
-// // // Pokemon get
-// API.get("1").then((pokemon) => {
-//   console.log(pokemon.species.name);
-// });
-
-// function fetchKantoPokemon() {
-//   fetch("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
-//     .then((response) => response.json())
-//     .then((allpokemon) => console.log(allpokemon));
-// }
-
-// fetchKantoPokemon();
-
 const pokedex = document.querySelector("#pokedex");
+let searchPokemon = document.querySelector("#input");
 
-const fetchData = () => {
-  fetch("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
-    .then((res) => res.json())
-    .then((data) => {
-      //   console.log("Sucesss fetch", data);
-      //results make array into readable format
-      //   pokeCards(data.results);
+// const fetchData = () => {
+//   fetch("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+//     .then((res) => res.json())
+//     .then((data) => {
+//       //   console.log("Sucesss fetch", data);
+//       //results make array into readable format
+//       //   pokeCards(data.results);
 
-      const fetches = data.results.map((p) => {
-        return fetch(p.url).then((res) => res.json());
-      });
-      Promise.all(fetches).then((res) => pokeCards(res));
-    });
+//       //   const fetches = data.results.map((p) => {
+//       //     return fetch(p.url).then((res) => res.json());
+//       //   });
+//       //   Promise.all(fetches).then((res) => pokeCards(res));
+//     });
+// };
+
+const fetchPokemon = () => {
+  const promises = [];
+  for (let i = 1; i <= 151; i++) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    promises.push(fetch(url).then((res) => res.json()));
+  }
+
+  Promise.all(promises).then((results) => {
+    const pokemon = results.map((result) => ({
+      name: result.name,
+      type: result.types.map((type) => type.type.name).join(" "),
+      id: result.id,
+      image: result.sprites.other.dream_world["front_default"],
+      //   type: result.types.map((type) => type.type.name).join(" "),
+    }));
+
+    pokeCards(pokemon);
+  });
 };
-//[0].types[1].type.name
-//data moves into data in parenthess in pokecards
 
+//data moves into data in parenthess in pokecards
 const pokeCards = (data) => {
   console.log(data);
-  //   pokedex.innerHTML = "something awesome";
-
   const cards = data
     .map((card) => {
-      return `<div id="card"><img src="${card.sprites.other.dream_world.front_default}" alt=""/>
+      return `<div id="card"><img src="${card.image}" alt=""/>
         <div class="pokemonName">${card.name}</div>
         <div>
-            <div class="type1">${card.types[0].type.name}</div>
+            <div class="type1">${card.type}</div>
         </div>
       </div>`;
     })
     .join("");
+
   pokedex.innerHTML = cards;
 };
 
-fetchData();
+fetchPokemon();
